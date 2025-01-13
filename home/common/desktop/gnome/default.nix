5555ca4e0bfdb1f019ff959cc8b012d7254fb135
Bryce Thompson <bterrantry@gmail.com>
2025-01-13 13:54:02 +0800

    Gnome configs

--- home/common/desktop/gnome/default.nix
+++ home/common/desktop/gnome/default.nix
@@ -1,10 +1,61 @@
 { config,  pkgs, ... }:
 {
-  programs.gnome-shell = {
-    enable = true;
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      gtk-theme = "adw-gtk3-dark";
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = false;
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Shift><Super>q"];
+      toggle-fullscreen = ["<Super>f"];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      move-to-workspace-1 = ["<Shift><Super>1"];
+      move-to-workspace-2 = ["<Shift><Super>2"];
+      move-to-workspace-3 = ["<Shift><Super>3"];
+      move-to-workspace-4 = ["<Shift><Super>4"];
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      dynamic-workspaces = false;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      help = [];
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 4;
+      workspace-names = [ "Main" ];
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
 
-    extensions = [
-      { package = pkgs.gnomeExtensions.dash-to-panel; }
-    ];
+      # `gnome-extensions list` to find the extension names 
+      enabled-extensions = [
+       "AlphabeticalAppGrid@stuarthayhurst"
+       "blur-my-shell@aunetx"
+       "Vitals@CoreCoding.com"
+      ];
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+    };
   };
+  
+  home.packages = with pkgs; [
+    adw-gtk3
+    gnomeExtensions.vitals
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.alphabetical-app-grid
+  ];
+
+  gtk.enable = true;
 }
--- host/common/desktop/gnome.nix
+++ host/common/desktop/gnome.nix
@@ -10,4 +10,25 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gedit
+    cheese
+    gnome-music
+    epiphany
+    geary
+    gnome-characters
+    tali
+    iagno
+    hitori
+    atomix
+    yelp
+    gnome-contacts
+    gnome-initial-setup
+  ]);
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+  ];
 }
